eb826c78bccda90c14c35d6c3c6bf8e7d93b30d7
suryabarisaputralerebulan <suryabarisaputralerebulan@gmail.com>
2022-09-20 10:34:17 +0700

    menambahkan percabangan menentukan genap/ganjil

---
+++ percabangan.pas
@@ -0,0 +1,18 @@
+Program Menentukan_genap_ganjil;
+uses crt;
+var     A:integer;
+begin   writeln ('PROGRAM PENENTU BILANGAN GANJIL GENAP');
+        writeln ('---------------------------------');
+        writeln ('-----------BY KASHIKI------------');
+        writeln('----------SELAMAT DATANG----------');
+        writeln('------SILAKAN MASUKAN ANGKA-------');
+        readln(A);
+        if(A mod 2=0) then
+    
+        writeln('bilangan tersebut genap') else
+        writeln('bilagan tersebut ganjil');
+        writeln ('-----------------------');
+        writeln ('-----------------------');
+        writeln ('thank you for using Our servises');
+        readln()
+        End.
